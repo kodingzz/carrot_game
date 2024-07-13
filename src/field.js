f2975e0d229595be81df338297810818d343a5f5
HyunooTv <96560273+kodingzz@users.noreply.github.com>
2024-07-14 00:33:26 +0900

    module-field
    당근, 벌레 랜덤 생성및 클릭에 관한 모듈 생성

---
+++ src/field.js
@@ -0,0 +1,62 @@
+'use-strict';
+
+
+
+export default class Field{
+    constructor(CARROT_COUNT,BUG_COUNT){
+        this.CARROT_COUNT = CARROT_COUNT;
+        this.BUG_COUNT =BUG_COUNT;
+        this.CARROT_COUNT= 5;
+        this.CARROT_SIZE=80;
+        this.gameBody =document.querySelector('.game__body');
+        this.gameBodyRect =this.gameBody.getBoundingClientRect();
+        this.score = 0;
+        
+        this.gameBody.addEventListener('click',(event)=>{
+           this.onClick && this.onClick(event);
+        });
+
+    }
+    setClickListener(onItemClick){
+        this.onItemClick= onItemClick;
+    }
+
+    onClick(event){
+        const target= event.target;
+        if(target.classList.contains('carrot')){
+             target.remove();
+            this.onItemClick&& this.onItemClick('carrot');
+        }
+        //  벌레 클릭시
+        else if(target.classList.contains('bug')){
+           
+            this.onItemClick&& this.onItemClick('bug');
+
+        }   
+
+    }
+
+    initRandom(){
+        this.gameBody.innerHTML='';
+        this.addItem('carrot',this.CARROT_COUNT,'img/carrot.png');
+        this.addItem('bug',this.BUG_COUNT,'img/bug.png');
+    }
+    
+    addItem(className,score,imgPath){
+               const gameBodyWidth =this.gameBodyRect.width;
+               const gameBodyHeight =this.gameBodyRect.height;
+               for(let i =0;i<score;i++){
+                   const img =document.createElement('img');
+                   img.src= imgPath;
+                   img.classList.add(className);
+                   img.style.position="absolute";
+                   img.style.cursor="pointer";
+                   const x= Math.random()*(gameBodyWidth-this.CARROT_SIZE);
+                   const y= Math.random()*(gameBodyHeight-this.CARROT_SIZE);
+                   img.style.left=`${x}px`;
+                   img.style.top=`${y}px`;
+                   this.gameBody.appendChild(img);
+               }
+    }
+
+}
--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 'use-strict'
 
 import  Popup  from './popup.js';
+import  Field  from './field.js';
+
 
 
 const gameBtn =document.querySelector('.game__btn');
-const gameBody =document.querySelector('.game__body');
-const gameBodyRect =gameBody.getBoundingClientRect();
 
 const gameTimer = document.querySelector('.timer');
 const gameTimerClock =document.querySelector('.timer__clock');
@@ -16,7 +16,6 @@ const gameCount =document.querySelector('.count');
 const BUG_COUNT= 5;
 const CARROT_COUNT= 5;
 const TIME = 5;
-const CARROT_SIZE= 80;
 
 
 // sound 변수
@@ -37,28 +36,27 @@ let started= false; // 게임 시작 유무
 
     gameReplayPopup.setClickListener(()=>startGame());
 
+    const gameField= new Field(CARROT_COUNT,BUG_COUNT);
+
+    gameField.setClickListener((item)=>onFieldClick(item));
 
 
-    gameBody.addEventListener('click',(event)=>onBodyClick(event));
 
+    function onFieldClick(item){
+                  if(item==='carrot'){
+                    playSound(carrotSound);
+                    gameCount.innerText= CARROT_COUNT- ++score;
+                        if(CARROT_COUNT===score){
+                            finishGame(true);
+                        }
+                }
+                //  벌레 클릭시
+                else if(item==='bug'){
+                    finishGame(false);
+                }   
 
-    function onBodyClick(event){
-            const target= event.target;
-              //  당근 클릭시
-              if(target.classList.contains('carrot')){
-                playSound(carrotSound);
-                gameCount.innerText= CARROT_COUNT- ++score;
-                target.remove();
-                    if(CARROT_COUNT===score){
-                        finishGame(true);
-                    }
-                
-            }
-            //  벌레 클릭시
-            else if(target.classList.contains('bug')){
-                finishGame(false);
-            }   
     }
+
 
     function finishGame(win){
         started= false;
@@ -97,7 +95,7 @@ let started= false; // 게임 시작 유무
         started=true;
         initGame();
         startTimer();
-        placeRandomly();
+        gameField.initRandom();
      }
 
      function startTimer(){
@@ -135,31 +133,7 @@ let started= false; // 게임 시작 유무
         score=0;
 
     }
-        // 당근과 벌레가  생성되어 랜덤하게 배치되는 함수
-     function placeRandomly(){     
-            gameBody.innerHTML='';
-            addItem('carrot',CARROT_COUNT,'img/carrot.png');
-            addItem('bug',BUG_COUNT,'img/bug.png');
-   }
-   
 
-   function addItem(className,score,imgPath){
-     
-       const gameBodyWidth =gameBodyRect.width;
-       const gameBodyHeight =gameBodyRect.height;
-       for(let i =0;i<score;i++){
-           const img =document.createElement('img');
-           img.src= imgPath;
-           img.classList.add(className);
-           img.style.position="absolute";
-           img.style.cursor="pointer";
-           const x= Math.random()*(gameBodyWidth-CARROT_SIZE);
-           const y= Math.random()*(gameBodyHeight-CARROT_SIZE);
-           img.style.left=`${x}px`;
-           img.style.top=`${y}px`;
-           gameBody.appendChild(img);
-       }
-   }
 
      function stopGame(){
         stopTimer();
